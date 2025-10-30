9b5efd48b0d72c383f79b52ce9fcfe0d18b3716d
fotimaruziyeva77 <shahloxasanova46@gmail.com>
2025-10-30 07:45:14 +0500

    vercel

--- lib/api-request.ts
+++ lib/api-request.ts
@@ -1,13 +1,12 @@
-const baseUrl=process.env.NEXT_PUBLIC_API_URL
+const baseUrl = process.env.NEXT_PUBLIC_API_URL
 
 export const API_SERVICE = {
-	blog:`${baseUrl}/blogs/`,
-	contact:`${baseUrl}/contacts/`,
-	successnumber:`${baseUrl}/successnumbers/`,
-	feedback:`${baseUrl}/clients/`,
-	edu:`${baseUrl}/educations/`,
-	category:`${baseUrl}/specialcategories/`,
-	service:`${baseUrl}/specialservices/`,
-	faqs:`${baseUrl}/faqs/`
-
+	blog: `${baseUrl}/blogs/`,
+	contact: `${baseUrl}/contacts/`,
+	successnumber: `${baseUrl}/successnumbers/`,
+	feedback: `${baseUrl}/clients/`,
+	edu: `${baseUrl}/educations/`,
+	category: `${baseUrl}/specialcategories/`,
+	service: `${baseUrl}/specialservices/`,
+	faqs: `${baseUrl}/faqs/`,
 }
